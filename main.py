c300cc77a737ab311b2aa3cda64d90b01dac5cc6
miri volozhinsky <mirivol@post.bgu.ac.il>
2023-08-21 21:36:36 +0300

    Add more text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Made by Miri Volozhinsky
 import PyPDF2
 import requests
 import re
@@ -5,15 +6,16 @@ from urllib.request import urlopen
 from pyquery import PyQuery as pq
 import pandas as pd
 
-year = '2024'
-semester = '1'
+year = '2024'  # שנה
+semester = '1'  # סמסטר
+department = "373"  # מחלקה
+year_degree = "4"  # שנה בתואר
+degree_level = "1"  # תואר ראשון
+gloabl_courses = "3"  # רק כללי
 
 
+# fetch a list of courses
 def fetch_courses():
-    department = "373"
-    year_degree = "4"
-    degree_level = "1"
-    gloabl_courses = "3"
     url = "https://reports4u.bgu.ac.il/reports/rwservlet?cmdkey=PROD&server=aristo4stu419c&report=acrr008w&desformat=pdf&DESTYPE=cache&P_YEAR={}&P_SEMESTER={}&P_INSTITUTION=0&P_DEGREE_LEVEL={}&P_DEPARTMENT={}&P_GLOBAL_COURSES={}&P_PATH=1&P_SPEC=&P_YEAR_DEGREE={}&P_SORT=1&P_WEB=1&P_SPECIAL_PATH=0&envid=NOENBIDI".format(
         year, semester, degree_level, department, gloabl_courses, year_degree)
     data = requests.get(url)
@@ -27,9 +29,11 @@ def fetch_courses():
         text = pageObj.extract_text()
         courses = courses + re.findall("[0-9]{3}.[0-9].[0-9]{4}", text)
     pdfFileObj.close()
+    print("הקורסים שאתה יכול לקחת הורדו לתקייה שלך לקובץ בשם courses.pdf")
     return courses
 
 
+# make a dict of {number: course_name}, not in use
 def fetch_names(courses):
     degress = set()
     for course in courses:
@@ -51,7 +55,8 @@ def fetch_names(courses):
     return coursesNames
 
 
-def fetch_course(courses):
+# fetch data for each course
+def fetch_courses_data(courses):
     coursesData = {}
     for course in courses:
         info = course.split('.')
@@ -66,7 +71,7 @@ def fetch_course(courses):
         data = text.split('\n')
         summary = ""
         try:
-            if data[18] != '0.0':
+            if data[18] != '0.0':  # only courses with credits
                 for i in range(len(data)):
                     if data[i] == 'תקציר:':
                         if data[i + 1] != 'ביבליוגרפיה:':
@@ -75,21 +80,32 @@ def fetch_course(courses):
                 location = re.findall("(?:מקוון|היברידי|בכיתה)", text)
                 location = set(location) if len(location) > 0 else {}
                 courseName = data[13]
-                print("קורס: " + courseName + " " + course + ":\nקישור:\n" + url + "\nלחץ על הקישור ומלא את הפרטים")
-                print("מבחן? (כן/לא)")
-                exam = input()
-                print("עבודות? (כן/לא)")
-                hw = input()
-                print("בוחן? (כן/לא)")
-                midterm = input()
-                print("נוכחות חובה? (כן/לא)")
-                mandatory = input()
-                coursesData[course] = {"שם": courseName, "מספר": course, "נקז": data[18], "תקציר": summary,
-                                       'מבחן': exam, 'בוחן': midterm, 'עבודות': hw, 'נוכחות חובה': mandatory,
-                                       'מיקום': location,
-                                       "קישור": url}
+                finishInput = False
+                while not finishInput:
+                    print("קורס: " + courseName + " " + course + ":\nקישור:\n" + url + "\nלחץ על הקישור ומלא את הפרטים")
+                    print("אם תטעה, ניתן בסוף לתקן")
+                    print("מבחן? (כן/לא/אנטר/כל דבר שתרצה)")
+                    exam = input()
+                    print("בוחן? (כן/לא/אנטר/כל דבר שתרצה)")
+                    midterm = input()
+                    print("עבודות? (כן/לא/אנטר/כל דבר שתרצה)")
+                    hw = input()
+                    print("נוכחות חובה? (כן/לא/אנטר/כל דבר שתרצה)")
+                    mandatory = input()
+                    print("אם טעית ואתה רוצה לכתוב מחדש את הקורס הזה תכתוב: שגיאה, אחרת תלחץ Enter")
+                    finish = input()
+                    finishInput = finish != "שגיאה"
+                    coursesData[course] = {"שם": courseName, "מספר": course, "נקז": data[18], "תקציר": summary,
+                                           'מבחן': exam, 'בוחן': midterm, 'עבודות': hw, 'נוכחות חובה': mandatory,
+                                           'מיקום': location,
+                                           "קישור": url}
         except Exception as e:
             print("course: " + course + " URL: " + url + "\nerror: " + str(e))
+    return coursesData
+
+
+# make a beautiful exel file
+def makeExel(coursesData):
     df = pd.DataFrame(coursesData.values(), index=coursesData.keys())
     df = df[['שם', 'מספר', 'נקז', 'מבחן', 'בוחן', 'עבודות', 'נוכחות חובה', 'מיקום', 'תקציר', 'קישור']]
     excel_filename = 'coursesSummary.xlsx'
@@ -102,19 +118,16 @@ def fetch_course(courses):
     wrap_format = excel_writer.book.add_format({'text_wrap': True})
     wrap_format.set_align('center')
     wrap_format.set_align('vcenter')
+    for i, column in enumerate(df.columns):
+        column_len = max(df[column].astype(str).apply(len).max(), len(column))
+        worksheet.set_column(i, i, column_len * 2, wrap_format)
     worksheet.set_column('A:A', 30, wrap_format)
-    column_len = max(df['מספר'].astype(str).apply(len).max(), len('מספר'))
-    worksheet.set_column('B:B', column_len, wrap_format)
-    column_len = max(df['נקז'].astype(str).apply(len).max(), len('נקז'))
-    worksheet.set_column('C:C', column_len, wrap_format)
-    column_len = max(df['מיקום'].astype(str).apply(len).max(), len('מיקום'))
-    worksheet.set_column('H:H', column_len, wrap_format)
     worksheet.set_column('I:I', 100, wrap_format)
-    column_len = max(df['קישור'].astype(str).apply(len).max(), len('קישור'))
-    worksheet.set_column('J:J', column_len, wrap_format)
     excel_writer.close()
+    print("קובץ אקסל נוצר בתקייה שלך בשם coursesSummary.xlsx")
 
 
 if __name__ == '__main__':
     courses = fetch_courses()
-    fetch_course(courses)
+    coursesData = fetch_courses_data(courses)
+    makeExel(coursesData)
